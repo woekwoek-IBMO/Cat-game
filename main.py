90746c6bbb3727d4813123ba5f041163940a951d
woekwoek-IBMO <ibrahim.mohammed@laetottenham.ac.uk>
2024-11-08 14:18:19 +0000

    started a new basic cat game

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
-catname=input("What is your cat's name?").capitalize()
-cat_colour=input("What colour would you like your cat to be?")
-intelligence=10
-energy=50
-weight=10
+cat_attributes = {
+    "intelligence": 10,
+    "energy": 50,
+    "weight": 10,
+}
 
+print("Welcome to my cat game!")
+
+name = input("Enter name:").capitalize()
+colour=input("Enter colour:")
+# ...
+
+# start the while loop
 while True:
-    action=input(f"What would you like to do with {catname}? (play, train, feed, sleep, view_stats,)").capitalize()
-    if action == "Play":
-        if energy<3:
-            print(f"{catname} doesnt have enough energy right now.")
-        else:
-            energy-=3
-    elif action == "Train":
-        if energy<5:
-            print(f"{catname} cannot train right now. Not enough energy.")
-        else:
-            energy-=5
-            intelligence+=3
-    elif action == "Feed":
-        energy+=4
-        weight+=2
-    elif action == "Sleep":
-        energy+=5
-    elif action == "View_stats":
-        print(f"{catname} is {cat_colour}\n{energy} energy left\nThey weigh {weight} kg\n{intelligence} intelligence")
+    # Finish the string below
+    option = input("What would you like to do? 1. Play with your cat 2. Train your cat 3. show stats")
+
+    if option == '1':
+        # change the cat's attributes here
+        pass
+    elif option == '2':
+        pass
+    # elif ...
+    else:
+        pass
+
+    # finish off the if statements below
+    if cat_attributes['energy'] < 0:
+        pass
+    # elif ...
+    
